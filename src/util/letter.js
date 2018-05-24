18a00ce9a12ca42b7a855c23c90c6e45fab6a431
Johannes Filter <hi@jfilter.de>
2018-05-24 19:48:38 +0200

    localize date

--- src/util/letter.js
+++ src/util/letter.js
@@ -7,7 +7,7 @@ darüber, ob bei Ihnen personenbezogene Daten über mich gespeichert sind. Falls
 verlange ich Auskunft über die Informationen nach Artikel 15, Absätze 1 und 2 DSGVO.
 Ich bitte um eine Bestätigung des Eingangs meines Antrags und gemäß Artikel 12 Absatz 3 
 DSGVO um Informationen zu den daraufhin ergriffenen Maßnahmen bis spätestens zum 
-folgenden Datum: {date}
+folgenden Datum: {date}.
 
 Bei Nichtbeachtung meiner Forderung werde ich mich an eine Datenschutzbehörde wenden. 
 Zudem behalte ich mir weitere rechtliche Schritte vor, die auch die Geltendmachung von 
